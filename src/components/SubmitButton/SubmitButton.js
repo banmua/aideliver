748d97e8cbeb951defcb838537b5b9a18e20ff65
Banmua <banmua72@gmail.com>
2020-04-27 16:36:12 -0700

    submit button

--- src/components/SubmitButton/SubmitButton.js
+++ src/components/SubmitButton/SubmitButton.js
@@ -3,6 +3,6 @@ import Button from '@material-ui/core/Button';
 
 export default ({style}) => {
     return (
-        <Button variant="contained" color="secondary" style={style}>Submit</Button>
+        <Button variant="contained" color="secondary" style={style}>Submit Your Order</Button>
     )
 }
