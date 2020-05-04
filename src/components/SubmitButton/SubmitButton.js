738fdac9862d57ae28acc1ea0cd9022d6298d994
Banmua <banmua72@gmail.com>
2020-05-03 21:29:09 -0700

    test submit

--- src/components/SubmitButton/SubmitButton.js
+++ src/components/SubmitButton/SubmitButton.js
@@ -3,6 +3,8 @@ import Button from '@material-ui/core/Button';
 
 export default ({style}) => {
     return (
-        <Button variant="contained" color="secondary" style={style}>Submit Your Order</Button>
+        <form action="mailto:phobalo72@gmail.com?cc=bar@example.com&subject=Order%20#123&body=Content" method="post" enctype="text/plain">
+        <Button variant="contained" type="submit" color="secondary" style={style}>Submit Your Order</Button>
+        </form>
     )
 }
