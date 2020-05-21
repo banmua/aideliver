ade99feba9558329d77c2c88d1f5d59196bcd722
Banmua <banmua72@gmail.com>
2020-05-21 14:17:15 -0700

    cancel disbaled

--- src/components/Admin/Status.js
+++ src/components/Admin/Status.js
@@ -35,7 +35,7 @@ const Status = (props) => {
         return (
             <div>
                 Status: {status}
-                {['completed', 'canceled'].includes(status) &&
+                {!['completed', 'canceled'].includes(status) &&
                     <EditIcon color='secondary' style={{paddingLeft: '20px'}} 
                             onClick={() => setIsOpen(true)} />}
             </div>
